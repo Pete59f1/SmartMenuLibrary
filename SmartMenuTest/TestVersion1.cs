1d50648fc5ecb2002e7ed2d8744b279303b848e9
Pete59f1 <pete59f1@edu.eal.dk>
2018-10-04 12:01:07 +0200

    Prøvede at lave tests

--- SmartMenuTest/TestVersion1.cs
+++ SmartMenuTest/TestVersion1.cs
@@ -7,10 +7,12 @@ namespace SmartMenuTest
     [TestClass]
     public class TestVersion1
     {
+
         [TestMethod]
-        public void loadMenu()
+        public void ActivateMenu()
         {
-            Assert.AreEqual("Vælg sprog - Choose language:\n Tryk D for Dansk\n Press E for English");
+            SmartMenu menu = new SmartMenu();
+            Assert.AreEqual("Vælg sprog - Choose language:\n Tryk D for Dansk\n Press E for English", menu.Activate());
         }
     }
 }
