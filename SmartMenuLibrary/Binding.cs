0971b6e457fc8a9b4c46030f5ff1a9d2256d99ab
Niclas Holmquist Jensen <nicl2445@edu.eal.dk>
2018-10-12 10:28:00 +0200

    tilføjet mulighed for egne variabler på clac app.

--- SmartMenuLibrary/Binding.cs
+++ SmartMenuLibrary/Binding.cs
@@ -12,6 +12,8 @@ namespace SmartMenuLibrary
     {
         public void chooseFucntion(string callID, int bind,string userInputSprog)
         {
+            string var1;
+            string var2;
             double[] numbers = new double[] { 4.5, 90.2, 2.9, 15.8, 938.0 };
             if (bind == 1)
             {
@@ -62,22 +64,40 @@ namespace SmartMenuLibrary
                 switch (callID)
                     {
                         case "1":
-                            Console.WriteLine(Calculator.Add(double.Parse(callID), double.Parse(callID)));
+                        Console.WriteLine("Indtast første variabel");
+                        var1 = Console.ReadLine();
+                        Console.WriteLine("Indtast anden varialbel");
+                        var2 = Console.ReadLine();
+
+                            Console.WriteLine(Calculator.Add(double.Parse(var1), double.Parse(var2)));
                             break;
 
                         case "2":
-                            Console.WriteLine(Calculator.Subtract(double.Parse(callID), double.Parse(callID)));
+                        Console.WriteLine("Indtast første variabel");
+                        var1 = Console.ReadLine();
+                        Console.WriteLine("Indtast anden varialbel");
+                        var2 = Console.ReadLine();
+                        Console.WriteLine(Calculator.Subtract(double.Parse(var1), double.Parse(var2)));
                             break;
 
                         case "3":
-                            Console.WriteLine(Calculator.Multiply(double.Parse(callID), double.Parse(callID)));
+                        Console.WriteLine("Indtast første variabel");
+                        var1 = Console.ReadLine();
+                        Console.WriteLine("Indtast anden varialbel");
+                        var2 = Console.ReadLine();
+                        Console.WriteLine(Calculator.Multiply(double.Parse(var1), double.Parse(var2)));
                             break;
 
                         case "4":
-                            Console.WriteLine(Calculator.Divide(double.Parse(callID), double.Parse(callID)));
+                        Console.WriteLine("Indtast første variabel");
+                        var1 = Console.ReadLine();
+                        Console.WriteLine("Indtast anden varialbel");
+                        var2 = Console.ReadLine();
+                        Console.WriteLine(Calculator.Divide(double.Parse(var1), double.Parse(var2)));
                             break;
 
                         case "5":
+
                             Console.WriteLine(Calculator.Sum(numbers));
                             break;
 
